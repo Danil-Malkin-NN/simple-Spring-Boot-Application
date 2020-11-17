d1593fa9212b0cfcc4ac7e227f139277a8c3812f
dmalkin <danil.malkin.ext@opencascade.com>
2020-11-17 16:24:21 +0300

    creating a variable in the dependency file, using it to output to the window (#3)

--- demo/src/main/java/com/example/demo/controller/SimpleController.java
+++ demo/src/main/java/com/example/demo/controller/SimpleController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SimpleController {
 
+    @Value("${app.version.server}")
+    private String name;
+
     @RequestMapping
     public String sayHello(){
-        return "Hello world";
+        return name;
     }
 }
