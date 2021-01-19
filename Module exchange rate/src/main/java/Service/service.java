862e747911f83dab11ae856af45fb6bbba2a399f
dmalkin <danil.malkin.ext@opencascade.com>
2021-01-19 14:52:43 +0300

    Init

---
+++ Module exchange rate/src/main/java/Service/service.java
@@ -0,0 +1,15 @@
+package Service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Service
+@RequestMapping("/exchange_rate")
+public class service {
+    //https://www.cbr-xml-daily.ru/daily_json.js
+
+    public void getExchangeRate() {
+
+    }
+
+}
